883e0adfda4f41647aae37ff24f9af3e0f340a23
Fatvallon <43625745+Fatvallon@users.noreply.github.com>
2018-11-10 16:07:40 +0200

    (dispatch) => (mod)

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 const fs = require('fs')
 const path = require('path')
 
-module.exports = function glyphs(dispatch) {
+module.exports = function glyphs(mod) {
 
   let glyphBuf = {unk: false, glyphs: []};
   let cache;
   let job = null;
   let jobs = ['Warrior', 'Lancer', 'Slayer', 'Berserker', 'Sorcerer', 'Archer', 'Priest', 'Mystic', 'Reaper', 'Gunner', 'Brawler', 'Ninja', 'Valkyrie'];
 
-  dispatch.hook('S_CREST_INFO', 2, (event) => {
+  mod.hook('S_CREST_INFO', 2, (event) => {
     glyphBuf.glyphs = [];
     for(let i of event.crests){
       if(!i.enable) continue;
@@ -16,7 +16,7 @@ module.exports = function glyphs(dispatch) {
     }
   });
 
-  dispatch.hook('S_CREST_APPLY', 2, (event) => {
+  mod.hook('S_CREST_APPLY', 2, (event) => {
     if(!event.enable){
       for(let i = 0; i < glyphBuf.glyphs.length; ++i){
         if(glyphBuf.glyphs[i].glyphId == event.id){
@@ -28,59 +28,59 @@ module.exports = function glyphs(dispatch) {
     glyphBuf.glyphs.push({glyphId: event.id});
   });
 
-  dispatch.hook('S_LOGIN', 10, (event) => {
+  mod.hook('S_LOGIN', 10, (event) => {
     job = (event.templateId - 10101) % 100;
   });
 
-  dispatch.command.add('glyph', (cmd, arg1) => {
+  mod.command.add('glyph', (cmd, arg1) => {
     switch(cmd){
       case 'save':
         fs.writeFile(path.join(__dirname, 'Glyphs', jobs[job], arg1+".json"), (JSON.stringify(glyphBuf, null, 4)), err => {
           if(err) {
-            dispatch.command.message('Glyph: Glyph folder not found.');
+            mod.command.message('Glyph: Glyph folder not found.');
             console.error(err);
             return;
           }
-          dispatch.command.message('Glyph: ' + arg1 + ' saved.');
+          mod.command.message('Glyph: ' + arg1 + ' saved.');
         });
         return;
       case 'load':
         fs.readFile(path.join(__dirname, 'Glyphs', jobs[job], arg1+".json"), function(err,data){
           if(err){
-            dispatch.command.message('Glyph: File not found.');
+            mod.command.message('Glyph: File not found.');
             console.error(err);
             return;
           }
           cache = JSON.parse(data);
-          dispatch.toServer('C_CREST_APPLY_LIST', 1, {unk: cache.unk, glyphs: cache.glyphs});
-          dispatch.command.message('Glyph: ' + arg1 + ' loaded.');
+          mod.toServer('C_CREST_APPLY_LIST', 1, {unk: cache.unk, glyphs: cache.glyphs});
+          mod.command.message('Glyph: ' + arg1 + ' loaded.');
         });
         return;
       case 'list':
         fs.readdir(path.join(__dirname, 'Glyphs', jobs[job]), (err, files) => {
           if(err){
-            dispatch.command.message('Glyph: Glyph folder not found.');
+            mod.command.message('Glyph: Glyph folder not found.');
             console.log(err); 
             return; 
           }
-          dispatch.command.message(' Glyph List ----');
+          mod.command.message(' Glyph List ----');
           for(let i = 0; i < files.length; ++i){
-            dispatch.command.message(' ' + (i+1) + ': ' + (files[i].slice(0, -5)));
+            mod.command.message(' ' + (i+1) + ': ' + (files[i].slice(0, -5)));
           }
         });
         return;
       case 'delete':
       fs.unlink(path.join(__dirname, 'Glyphs', arg1+".json"), err => {
         if(err){
-          dispatch.command.message('Glyph: File not found.');
+          mod.command.message('Glyph: File not found.');
           console.error(err);
           return;
         }
-        dispatch.command.message('Glyph: ' + arg1 + ' deleted.');
+        mod.command.message('Glyph: ' + arg1 + ' deleted.');
       });
       return;
       default: 
-        dispatch.command.message('Glyph: Unknown Command.');
+        mod.command.message('Glyph: Unknown Command.');
         return;
     }
   });
